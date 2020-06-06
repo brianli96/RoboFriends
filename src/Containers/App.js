414e6580c1d42963c99c8a7a66026b7e74630db4
brianli96 <65915100+brianli96@users.noreply.github.com>
2020-06-06 04:05:50 -0500

    I suck at coding

---
+++ src/Containers/App.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import CardList from '../Components/cardList';
+import SearchBox from '../Components/searchBox.js';
+import './App.css';
+import Scroll from '../Components/Scroll.js';
+import ErrorBoundary from '../Components/ErrorBoundary';
+
+class App extends Component
+{
+    constructor()
+    {
+        super()
+        this.state =
+        {
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount()
+    {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then (response => response.json())
+        .then (users => this.setState({robots: users}));  
+    }
+
+
+    onSearchChange = (e) =>
+    {
+        this.setState({searchfield: e.target.value})
+
+    }
+
+
+    render ()
+        {
+            const {robots, searchfield} =this.state;
+            const filteredRobots = robots.filter(robot =>
+                {
+                    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+                })
+            return (
+                <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                <ErrorBoundary>
+                <CardList robots = {filteredRobots}/>
+                </ErrorBoundary>
+                </Scroll>
+                </div>
+            );
+        }
+
+}
+
+export default App;
